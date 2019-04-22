45397f0f826b58effe45e1a674971785dd806268
Vy Ta <vyta@microsoft.com>
2019-04-22 11:37:08 -0700

    Adding services and dockerfiles

---
+++ src/poi/README.md
@@ -0,0 +1,20 @@
+
+# POI Service
+
+## Overview
+
+## Build the Application
+
+## Tests
+
+### Running the unit tests
+
+### Running the Integration Test
+
+## References
+
+* [Web API](https://docs.microsoft.com/en-us/aspnet/core/tutorials/web-api-vsc?view=aspnetcore-2.1)
+* [Unit Testing](https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test?view=aspnetcore-2.1)
+* [Integration Testing](https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-2.1)
+* [Example - How to Debug .NET Core Xunit Tests](https://github.com/nickolasacosta/dotnetcore-xunit-debugging)
+* [Logging in ASP.NET Core](https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1&tabs=aspnetcore2x)
---
+++ src/tripviewer/web/Models/Stores/UserStore.cs
@@ -0,0 +1,41 @@
+﻿namespace Simulator.DataStore.Stores
+{
+    using Simulator.DataObjects;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public class UserStore : BaseStore//, IBaseStore<User>
+    {
+        public UserStore(string EndPoint)
+        {
+            base.InitializeStore(EndPoint);
+
+        }
+        public async Task<User> GetItemAsync(string id)
+        {
+            User user = null;
+            HttpResponseMessage response = await Client.GetAsync($"/api/user/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                response.Content.Headers.ContentType.MediaType = "application/json";
+                user = await response.Content.ReadAsAsync<User>();
+            }
+            return user;
+        }
+
+        public async Task<List<User>> GetItemsAsync()
+        {
+            List<User> users = null;
+            HttpResponseMessage response = await Client.GetAsync("api/user/");
+            if (response.IsSuccessStatusCode)
+            {
+                response.Content.Headers.ContentType.MediaType = "application/json";
+                users = await response.Content.ReadAsAsync<List<User>>();
+            }
+            return users;
+        }
+
+        
+    }
+}
