4af3871f9fbff354ad03030da4fba65d73d6faf6
Paul Bouwer <paul.bouwer@gmail.com>
2019-07-08 17:16:22 +1000

    Add file based config, update README and Dockerfile.

--- src/userprofile/server.js
+++ src/userprofile/server.js
@@ -1,24 +1,51 @@
 'use strict';
 
+var fs = require('fs');
 var Http = require('http');
 var Express = require('express');
 var BodyParser = require('body-parser');
 var Swaggerize = require('swaggerize-express');
 var Path = require('path');
 var tediousExpress = require('express4-tedious');
-var sqlConfig = require('./config/sqlConfig');
 var morgan = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./config/swagger.json');
 
+// Configuration and potential overrides
+function getConfigValue(name, defaultValue) {
+    var value = "";
+
+    if (process.env.CONFIG_VARIABLE_LOCATION) {
+        var filePath = Path.join(process.env.CONFIG_VARIABLE_LOCATION, name);
+        value = (fs.existsSync(filePath)) ? fs.readFileSync(filePath, 'utf8').trim() : defaultValue;
+    } else {
+        value = process.env[name] || defaultValue;
+    }
+    return value;
+}
+var port = process.env.PORT || 80;
+var sqlUser = getConfigValue('SQL_USER', 'sqladmin');
+var sqlPassword = getConfigValue('SQL_PASSWORD', '');
+var sqlServer = getConfigValue('SQL_SERVER', '');
+var sqlDBName = getConfigValue('SQL_DBNAME', 'mydrivingDB');
+
 var App = Express();
-
 var Server = Http.createServer(App);
-
 var logger = morgan(':remote-addr [:date[web]] :method :url HTTP/:http-version :status :res[content-length] :referrer :user-agent :response-time ms');
-
 App.use(logger);
 
+var sqlConfig = {
+    userName:   sqlUser,
+    password:   sqlPassword,
+    server:     sqlServer,
+    options: {
+        encrypt: true,
+        database: sqlDBName,
+        MultipleActiveResultSets: false,
+        TrustServerCertificate: false,
+        rowCollectionOnDone: true
+    }
+};
 App.use(function (req, res, next) {
     req.sql = tediousExpress(sqlConfig);
     next();
@@ -33,10 +60,9 @@ App.use(Swaggerize({
     api: Path.resolve('./config/swagger.json'),
     handlers: Path.resolve('./handlers')
 }));
-
 App.use('/api/docs/user', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-Server.listen(8080, function () {
+Server.listen(port, function () {
     App.swagger.api.host = this.address().address + ':' + this.address().port;
     /* eslint-disable no-console */
     console.log('App running on %s:%d', this.address().address, this.address().port);
